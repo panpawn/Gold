70fbde6359b207ca9e118cd3670a10ab033cb05b
Blizzard <bronyanthony@gmail.com>
2014-08-08 22:06:04 -0400

    Room Owner in lobby is bad idea.

--- commands.js
+++ commands.js
@@ -4011,6 +4011,7 @@ var commands = exports.commands = {
 			return this.sendReply( '/hideauth - access denied.');
 
 		var tar = ' ';
+		if (target == '#') return false;
 		if(target){
 			target = target.trim();
 			if(config.groupsranking.indexOf(target) > -1){
