9a0630f59acfc99e2c4f30809b2846708957bc8f
panpawn <thebestintheworld12321@gmail.com>
2016-06-09 13:02:46 -0400

    Tournaments: use Wisp.nameColor for name displays

--- tournaments/index.js
+++ tournaments/index.js
@@ -858,8 +858,8 @@ class Tournament {
 			if (firstMoney) {
 				if (firstMoney > 1) firstBuck = 'bucks';
 				if (secondMoney > 1) secondBuck = 'bucks';
-				this.room.add('|raw|<b><font color="' + Wisp.hashColor(winner) + '">' + Tools.escapeHTML(winner) + '</font> has also won <font color=#b30000>' + firstMoney + '</font> ' + firstBuck + ' for winning the tournament!</b>');
-				if (runnerUp) this.room.add('|raw|<b><font color="' + Wisp.hashColor(runnerUp) + '">' + Tools.escapeHTML(runnerUp) + '</font> has also won <font color=#b30000>' + secondMoney + '</font> ' + secondBuck + ' for coming in second!</b>');
+				this.room.add('|raw|<b>' + Wisp.nameColor(winner, false) + ' has also won <font color=#b30000>' + firstMoney + '</font> ' + firstBuck + ' for winning the tournament!</b>');
+				if (runnerUp) this.room.add('|raw|<b>' + Wisp.nameColor(runnerUp, false) + ' has also won <font color=#b30000>' + secondMoney + '</font> ' + secondBuck + ' for coming in second!</b>');
 				Economy.writeMoney(toId(winner), firstMoney, () => {
 					Economy.readMoney(toId(winner), newMoney => {
 						Economy.logTransaction(winner + ' has won ' + firstMoney + ' ' + firstBuck + ' from a tournament in ' + this.room.title + '. They now have ' + newMoney);
@@ -881,7 +881,7 @@ class Tournament {
 			Wisp.addTourWin(winner);
 			try {
 				let tourRarity = Wisp.tourCard(tourSize, toId(winner));
-				this.room.addRaw("<b><font color='" + Wisp.hashColor(winner) + "'>" + Tools.escapeHTML(winner) + "</font> has also won a <font color=" + tourRarity[0] + ">" + tourRarity[1] + "</font> card: <button class='tourcard-btn' style='border-radius: 20px; box-shadow: 1px 1px rgba(255, 255, 255, 0.3) inset, -1px -1px rgba(0, 0, 0, 0.2) inset, 2px 2px 2px rgba(0, 0, 0, 0.5);' name='send' value='/card " + tourRarity[2] + "'>" + tourRarity[3] + "</button> from the tournament.");
+				this.room.addRaw("<b>" + Wisp.nameColor(winner, false) + " has also won a <font color=" + tourRarity[0] + ">" + tourRarity[1] + "</font> card: <button class='tourcard-btn' style='border-radius: 20px; box-shadow: 1px 1px rgba(255, 255, 255, 0.3) inset, -1px -1px rgba(0, 0, 0, 0.2) inset, 2px 2px 2px rgba(0, 0, 0, 0.5);' name='send' value='/card " + tourRarity[2] + "'>" + tourRarity[3] + "</button> from the tournament.</b>");
 			} catch (e) {
 				console.log('Error giving cards for tournaments: ' + e.stack);
 			}
