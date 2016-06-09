d93a6b454efa52f2cfbb5c4641462815908a2114
panpawn <rocketboy231@gmail.com>
2016-06-09 00:45:46 +0000

    Add check if someone already has a custom color in /customcolor

--- chat-plugins/colors.js
+++ chat-plugins/colors.js
@@ -69,7 +69,7 @@ exports.commands = {
 			if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup(user.name + " removed your custom color.");
 			return;
 		}
-
+		if (customColors[toId(target[0])]) return this.errorReply("This user already has a custom color.  Do /customcolor delete, [user] and then set their new color.");
 		this.sendReply("|raw|You have given <b><font color=" + target[1] + ">" + Tools.escapeHTML(target[0]) + "</font></b> a custom color.");
 		Rooms('staff').add('|raw|' + Tools.escapeHTML(target[0]) + " has recieved a <b><font color=" + target[1] + ">custom color</fon></b> from " + Tools.escapeHTML(user.name) + ".").update();
 		this.privateModCommand("(" + target[0] + " has recieved custom color: '" + target[1] + "' from " + user.name + ".)");
