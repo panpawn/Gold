81eaa20302b1df2ef15f2b4563dea42ffd259c7a
panpawn <rocketboy231@gmail.com>
2016-06-11 16:54:26 +0000

    Fix emote greentext in pms and formatting bug

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -30,12 +30,13 @@ Gold.emoticons = {
 				'<img src="' + self.chatEmotes[match] + '" title="' + match + '"/>' :
 				match;
 		});
+		if (message === text) return text;
 		// PS Formatting
 		message = message.replace(/\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>'); // italics
 		message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, '<b>$1</b>'); // bold
 		message = message.replace(/\~\~([^< ](?:[^<]*?[^< ])?)\~\~/g, '<strike>$1</strike>'); // strikethrough
 		message = Autolinker.link(message, {stripPrefix: false, phone: false, twitter: false}); // hyperlinking
-		if (message.substr(0, 4) === '&gt;') message = '<span class="greentext">' + message + '</span>'; // greentext
+		if (message.substr(0, 4) === '&gt;' || message.substr(0, 1) === '>') message = '<span class="greentext">' + message + '</span>'; // greentext
 		return message;
 	},
 	checkEmoteModchat: function(user, room) {
