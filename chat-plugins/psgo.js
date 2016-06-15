c40d11d1b44cfffcb329aefb4ce67c4b0c2b82c9
panpawn <rocketboy231@gmail.com>
2016-06-15 22:32:55 +0000

    Missed color coded name in PSGO

--- chat-plugins/psgo.js
+++ chat-plugins/psgo.js
@@ -565,7 +565,7 @@ exports.commands = {
 
 		// send messages
 		this.sendReply("Your trade has been taken submitted.");
-		if (Users.get(targetUser)) Users.get(targetUser).send("|pm|~Card Shop [Do not Reply]|" + targetUser + "|/html <div class=\"broadcast-green\">" + Tools.escapeHTML(user.name) + " has initiated a trade with you.  Click <button name=\"send\" value=\"/trades last\">here</button> or use <b>/trades</b> to view your pending trade requests.</div>");
+		if (Users.get(targetUser)) Users.get(targetUser).send("|pm|~Card Shop [Do not Reply]|" + targetUser + "|/html <div class=\"broadcast-green\">" + Gold.nameColor(user.name, true) + " has initiated a trade with you.  Click <button name=\"send\" value=\"/trades last\">here</button> or use <b>/trades</b> to view your pending trade requests.</div>");
 		user.send("|pm|~Card Shop [Do not Reply]|" + user.userid + "|/html <div class=\"broadcast-green\">Your trade with " + Gold.nameColor(targetUser, true) + " has been initiated.  Click <button name=\"send\" value=\"/trades last\">here</button> or use <b>/trades</b> to view your pending trade requests.</div>");
 	},
 
