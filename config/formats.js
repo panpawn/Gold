ba224ce0e8d0defe9457eb2058807b4d4f4eb936
UserLeafy <2Dover3Dthedon@gmail.com>
2016-06-28 14:01:37 -0700

    Add ORAS 2U

--- config/formats.js
+++ config/formats.js
@@ -39,14 +39,14 @@ exports.Formats = [
 	},
 	{
 		name: "ORAS 2U",
-                 desc: [
-                      "&bullet; <a href=\"http://www.pokemonperfect.com/forums/index.php?threads/oras-2u-discussion-thread.3865/\">np: ORAS 2U</a>",
-                ],
-		section: "ORAS Singles",
-
-		ruleset: ['Pokemon', 'Ubers', 'Team Preview'],
-		banlist: ['Arceus', 'Arceus-Dark', 'Arceus-Electric', 'Arceus-Fairy', 'Arceus-Ghost', 'Arceus-Grass', 'Arceus-Ground', 'Arceus-Rock', 'Arceus-Water', 'Blaziken-Mega', 'Darkrai', 'Deoxys-Attack', 'Deoxys-Speed', 'Dialga', 'Diancie-Mega', 'Ferrothorn', 'Genesect', 'Giratina-Origin', 'Gliscor', 'Groudon', 'Ho-oh', 'Kangaskhan-Mega', 'Klefki', 'Kyogre', 'Landorus-Therian', 'Latias', 'Latios', 'Lucario-Mega', 'Lugia', 'Mewtwo', 'Palkia', 'Rayquaza', 'Sableye-Mega', 'Shaymin-Sky', 'Tyranitar', 'Xerneas', 'Yveltal', 'Zekrom'],
-         },
+						desc: [
+							"&bullet; <a href=\"http://www.pokemonperfect.com/forums/index.php?threads/oras-2u-discussion-thread.3865/\">np: ORAS 2U</a>",
+                  ],
+ 		section: "ORAS Singles",
+ 
+ 		ruleset: ['Pokemon', 'Ubers', 'Team Preview'],
+ 		banlist: ['Arceus', 'Arceus-Dark', 'Arceus-Electric', 'Arceus-Fairy', 'Arceus-Ghost', 'Arceus-Grass', 'Arceus-Ground', 'Arceus-Rock', 'Arceus-Water', 'Blaziken-Mega', 'Darkrai', 'Deoxys-Attack', 'Deoxys-Speed', 'Dialga', 'Diancie-Mega', 'Ferrothorn', 'Genesect', 'Giratina-Origin', 'Gliscor', 'Groudon', 'Ho-oh', 'Kangaskhan-Mega', 'Klefki', 'Kyogre', 'Landorus-Therian', 'Latias', 'Latios', 'Lucario-Mega', 'Lugia', 'Mewtwo', 'Palkia', 'Rayquaza', 'Sableye-Mega', 'Shaymin-Sky', 'Tyranitar', 'Xerneas', 'Yveltal', 'Zekrom'],
+	},
 	{
 		name: "Ubers",
 		desc: [
