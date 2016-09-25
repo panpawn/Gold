9e12b117eb10a23570f2c161761463f0c59092ce
panpawn <rocketboy231@gmail.com>
2016-09-25 23:35:51 +0000

    Update various things within /declareaotd command

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -975,15 +975,15 @@ exports.commands = {
 		};
 	})(),
 	declareaotd: function (target, room, user) {
-		if (room.id !== 'lobby') return this.sendReply("The command must be used in Lobby.");
-		if (!user.can('broadcast', null, room)) return this.sendReply('You do not have enough authority to use this command.');
+		if (room.id !== 'lobby') return this.errorReply("The command must be used in Lobby.");
+		if (!this.can('broadcast', null, room)) return false;
 		if (!this.canTalk()) return false;
-		this.add(
+		room.add(
 			'|raw|<div class="broadcast-blue"><b>AOTD has begun in GoldenrodRadioTower! ' +
 			'<button name="joinRoom" value="goldenrodradiotower" target="_blank">Join now</button> to nominate your favorite artist for AOTD to be featured on the ' +
 			'official page next to your name for a chance to win the monthly prize at the end of the month!</b></div>'
-		);
-		this.logModCommand(user.name + " used declareaotd.");
+		).update();
+		this.privateModCommand("(" + user.name + " used declareaotd.)");
 	},
 	hideadmin: function (target, room, user) {
 		if (!this.can('hotpatch')) return false;
