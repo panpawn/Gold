aea3081f64f854bd1f7c54bbefb385114982797e
Nickoopa <nicolasdnetto@gmail.com>
2016-10-16 14:10:04 -0200

    Create mod for MCPX
    That is to ease code for allowing Eviolite on LC Pokémon from higher tiers.

---
+++ mods/mcpx/formats-data.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.BattleFormatsData = {
+	vullaby: {
+		inherit: true,
+		tier: 'LC',
+	},
+	ferroseed: {
+		inherit: true,
+		tier: 'LC',
+	},
+	pawniard: {
+		inherit: true,
+		tier: 'LC',
+	},
+};
