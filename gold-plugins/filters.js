31e952464e9f8e1253363516951fdebfe2b2169e
panpawn <jeremypiemonte@gmail.com>
2017-04-02 03:50:42 +0000

    Fix first filter

--- gold-plugins/filters.js
+++ gold-plugins/filters.js
@@ -88,8 +88,7 @@ exports.chatfilter = function (message, user, room, connection, targetUser) {
 		let firsts = ['first', 'f1rst', '1', '1st', 'f1r5t', 'fir5t'];
 		let regEx = new RegExp(firsts.join('|'),"g");
 		if (message.toLowerCase().match(regEx)) {
-			user.sendTo(room, "Wow, you're first? What a great acomplishment. Seriously, great job. Yeah, being first isn't cool anymore.");
-			return false;
+			return user.sendTo(room, "Wow, you're first? What a great acomplishment. Seriously, great job. Yeah, being first isn't cool anymore.");
 		}
 	}
 
