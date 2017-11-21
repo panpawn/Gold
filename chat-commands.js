5193a3fcab64247c3b824c24facd48a4cc0a4cfa
Guangcong Luo <guangcongluo@gmail.com>
2017-11-21 06:26:17 -0500

    Add "Wally" hidden avatar

--- chat-commands.js
+++ chat-commands.js
@@ -381,6 +381,7 @@ exports.commands = {
 			yellow: '#yellow',
 			zinnia: '#zinnia',
 			clemont: '#clemont',
+			wally: '#wally',
 		};
 		if (avatarTable.hasOwnProperty(avatarid)) {
 			avatar = avatarTable[avatarid];
