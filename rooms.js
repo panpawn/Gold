814a75c976b2ca027400f2d7c1dda4c8105b7162
Guangcong Luo <guangcongluo@gmail.com>
2017-12-17 17:34:52 -0600

    Update onJoin/onRename/onLeave for rooms

--- rooms.js
+++ rooms.js
@@ -1171,7 +1171,7 @@ class BasicChatRoom extends BasicRoom {
 	 */
 	onJoin(user, connection) {
 		if (!user) return false; // ???
-		if (this.users[user.userid]) return user;
+		if (this.users[user.userid]) return false;
 
 		if (user.named && !user.hidden) {
 			this.reportJoin('j', user.getIdentity(this.id));
@@ -1181,7 +1181,7 @@ class BasicChatRoom extends BasicRoom {
 		this.userCount++;
 
 		if (this.game && this.game.onJoin) this.game.onJoin(user, connection);
-		return user;
+		return true;
 	}
 	/**
 	 * @param {User} user
@@ -1189,6 +1189,9 @@ class BasicChatRoom extends BasicRoom {
 	 * @param {boolean} joining
 	 */
 	onRename(user, oldid, joining) {
+		if (user.userid === oldid) {
+			return this.onUpdateIdentity(user);
+		}
 		if (!this.users[oldid]) {
 			Monitor.crashlog(new Error(`user ${oldid} not in room ${this.id}`));
 		}
@@ -1206,7 +1209,7 @@ class BasicChatRoom extends BasicRoom {
 			this.reportJoin('n', user.getIdentity(this.id) + '|' + oldid);
 		}
 		if (this.poll && user.userid in this.poll.voters) this.poll.updateFor(user);
-		return user;
+		return true;
 	}
 	/**
 	 * onRename, but without a userid change
@@ -1217,16 +1220,17 @@ class BasicChatRoom extends BasicRoom {
 			if (!this.users[user.userid]) return false;
 			this.reportJoin('n', user.getIdentity(this.id) + '|' + user.userid);
 		}
+		return true;
 	}
 	/**
 	 * @param {User} user
 	 */
 	onLeave(user) {
-		if (!user) return; // ...
+		if (!user) return false; // ...
 
 		if (!(user.userid in this.users)) {
 			Monitor.crashlog(new Error(`user ${user.userid} already left`));
-			return;
+			return false;
 		}
 		delete this.users[user.userid];
 		this.userCount--;
@@ -1235,6 +1239,7 @@ class BasicChatRoom extends BasicRoom {
 			this.reportJoin('l', user.getIdentity(this.id));
 		}
 		if (this.game && this.game.onLeave) this.game.onLeave(user);
+		return true;
 	}
 	destroy() {
 		// deallocate ourself
