f140a4103a91c1352db81b6fbbf03d9f32e80dfd
The Immortal <the_immortal123@live.com>
2017-12-19 11:53:24 +0800

    Random Battle: Add custom level for Dugtrio

--- data/random-teams.js
+++ data/random-teams.js
@@ -1455,7 +1455,7 @@ class RandomTeams extends Dex.ModdedDex {
 		};
 		let customScale = {
 			// Banned Abilities
-			Gothitelle: 77, Politoed: 79, Wobbuffet: 77,
+			Dugtrio: 77, Gothitelle: 77, Politoed: 79, Wobbuffet: 77,
 
 			// Holistic judgement
 			Unown: 100,
